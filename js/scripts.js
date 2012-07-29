2b4942e2e5d7e84a247042a3a56650cae1f1d2d3
TJ Lavelle <tjlav5@gmail.com>
2012-07-29 19:21:53 -0400

    logic bug

--- js/scripts.js
+++ js/scripts.js
@@ -39,12 +39,48 @@ $(function() {
 		/**** Initialize 0-key for balance ****/
 		if (localStorage.length == 0)
 			localStorage.setItem("balance",0);
+		$("#balance").html("Balance: $"+localStorage.getItem("balance"));
 
 		/**** Load data from local storage on page load ****/
-		if (localStorage.length > 0) {
-			//$("tbody").html
+		if (localStorage.length) {
+			htmlData = "";
+			for (var i = 0; i < (localStorage.length-1)/4; i++) {
+				// alert(localStorage.getItem(i+".note"));
+				// alert(localStorage.getItem(i+".expense"));
+				// alert(localStorage.getItem(i+".income"));
+				// alert(localStorage.getItem(i+".date"));
+           		htmlData += "<tr><td>"+i+"</td><td>"+localStorage.getItem(i+".note")+"</td><td>"+localStorage.getItem(i+".expense")+"</td><td>"+localStorage.getItem(i+".income")+"</td><td>"+localStorage.getItem(i+".date")+"</td><td></td></tr>";
+      		}
+      		$(".data").html(htmlData);
 		}
 });
+
+// Check if string is currency
+var isCurrency_re    = /^\s*(\+|-)?((\d+(\.\d\d)?)|(\.\d\d))\s*$/;
+function isCurrency (s) {
+		return String(s).search (isCurrency_re) != -1
+}
+
+// Check if note is non-blank
+var isNonblank_re    = /\S/;
+function isNonblank (s) {
+	return String (s).search (isNonblank_re) != -1
+}
+
+// Check if either income or expense
+function isExpOrInc () {
+	if (( isNonblank(expenseIn.val()) && !isNonblank(incomeIn.val()) ) || 
+		( !isNonblank(expenseIn.val()) && isNonblank(incomeIn.val()) ))
+			return true;
+	else
+		return false;
+}
+
+// Checck if date is MM/DD/YYYY
+function isGoodDate(dt){
+	var reGoodDate = /^((0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])[- /.](19|20)?[0-9]{2})*$/;
+	return reGoodDate.test(dt);
+}
 
 function validate() {
 	console.log("validating");
@@ -53,33 +89,6 @@ function validate() {
 	console.log("expense: " + expenseIn.val());
 	console.log("income: " + incomeIn.val());
 	console.log("date: " + dateIn.val());
-
-	// Check if string is currency
-	var isCurrency_re    = /^\s*(\+|-)?((\d+(\.\d\d)?)|(\.\d\d))\s*$/;
-	function isCurrency (s) {
-   		return String(s).search (isCurrency_re) != -1
-	}
-
-	// Check if note is non-blank
-	var isNonblank_re    = /\S/;
-	function isNonblank (s) {
-		return String (s).search (isNonblank_re) != -1
-	}
-
-	// Check if either income or expense
-	function isExpOrInc () {
-		if (( isNonblank(expenseIn.val()) && !isNonblank(incomeIn.val()) ) || 
-			( !isNonblank(expenseIn.val()) && isNonblank(incomeIn.val()) ))
-				return true;
-		else
-			return false;
-	}
-
-	// Checck if date is MM/DD/YYYY
-	function isGoodDate(dt){
-    	var reGoodDate = /^((0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])[- /.](19|20)?[0-9]{2})*$/;
-    	return reGoodDate.test(dt);
-	}
 
 	console.log("note non-blank: " + isNonblank(noteIn.val()));
 	console.log("expense is currency: " + isCurrency(expenseIn.val()));
@@ -101,4 +110,15 @@ function save() {
 	localStorage.setItem(Math.floor((localStorage.length-1)/4)+".expense",expenseIn.val());
 	localStorage.setItem(Math.floor((localStorage.length-1)/4)+".income",incomeIn.val());
 	localStorage.setItem(Math.floor((localStorage.length-1)/4)+".date",dateIn.val());
+
+	oldBalance = parseInt(localStorage.getItem("balance"));
+	newItem = 0;
+	if (isCurrency(expenseIn.val()))
+		newItem = -expenseIn.val();
+	else
+		newItem = incomeIn.val();
+	newBalance = parseFloat(oldBalance) + parseFloat(newItem);
+	console.log(newBalance);
+	localStorage.setItem("balance", newBalance);
+	$("#balance").html("Balance: $"+localStorage.getItem("balance"));
 }
