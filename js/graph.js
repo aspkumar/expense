cc4405c94b9896ab95aef081f79655813db60ec1
TJ Lavelle <tjlav5@gmail.com>
2012-07-31 20:14:05 -0400

    graph w/o avgs

--- js/graph.js
+++ js/graph.js
@@ -1,21 +1,98 @@
+var pointsExp = [];
+var pointsInc = [];
+var graphTitle = "";
+
 $(function() {
 	$("#graphBtn").on("click", function() {
 		$('#graphs').modal('toggle');
-		var cosPoints = []; 
-		  for (var i=0; i<2*Math.PI; i+=0.1){ 
-		     cosPoints.push([i, Math.cos(i)]); 
-		  } 
-		  var plot1 = $.jqplot('chart', [cosPoints], {  
-		      series:[{showMarker:false}],
-		      axes:{
-		        xaxis:{
-		          label:'Angle (radians)'
-		        },
-		        yaxis:{
-		          label:'Cosine'
-		        }
-		      }
-		  });
-		  
+		populate1Month();
+		chartIt();
 	});
 });
+
+// monPoints.push([i, Math.cos(i)]);
+
+function populate1Month() {
+	var monExpPoints = [];
+	var monIncPoints = [];
+	var monExpCount = [];
+	var monIncCount = [];
+	graphTitle = "30 Days"
+	for (var i = 0; i < (localStorage.length-1)/5; i++) {
+		// plot if point is active
+		if ( localStorage.getItem(i+".active") == "true" ) {
+			date = new Date(localStorage.getItem(i+".date"));
+			if ( localStorage.getItem(i+".expense") != "" ) {
+				monExpPoints.push([date, parseFloat(localStorage.getItem(i+".expense"))]);
+			} else {
+				monIncPoints.push([date, parseFloat(localStorage.getItem(i+".income"))]);
+			}
+		}
+	}
+	pointsExp = monExpPoints;
+	pointsInc = monIncPoints;
+}
+
+function populate3Month() {
+
+}
+
+function populate6Month() {
+
+}
+
+function populate12Month() {
+
+}
+
+function populateAll() {
+
+}
+
+function chartIt() {
+	var plot1 = $.jqplot('chart', [pointsExp, pointsInc], {
+		title: graphTitle,
+		series:[{showMarker:true}],
+		axes:{
+			xaxis:{
+				label:'Date',
+				rendererOptions: { forceTickAt0: true, forceTickAt100: false },
+				padMin: 0,
+				renderer:$.jqplot.DateAxisRenderer,
+				tickOptions:{
+					formatString:'%b&nbsp;%#d'
+				} 
+			},
+			yaxis:{
+				tickOptions:{
+					formatString:'$%.2f'
+				}
+			}
+		},
+		highlighter: {
+			show: true,
+			sizeAdjust: 7.5
+		},
+		seriesColors:['#C7754C', '#73C774'],
+		seriesDefaults: {
+			rendererOptions: {
+				smooth: true,
+				varyByColor: true
+			}
+		},
+		canvasOverlay: {
+			show: true,
+			objects: [
+				{horizontalLine: {
+					name: 'pebbles',
+					y: 0,
+					lineWidth: 2,
+					color: 'rgb(0, 0, 0)',
+					shadow: false,
+					lineCap: 'butt',
+					xOffset: 0
+				}}
+			]
+		}
+	});
+}
